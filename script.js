f97b4c7f78a15a5c991f034dd8b6ecd0813a783e
imsdolta <imsdolta@gmail.com>
2020-05-14 20:22:59 +0530

    :tada: this is first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var yesButton = document.getElementById("yes");
+var noButton = document.getElementById("no");
+
+yesButton.addEventListener('click', function(){
+    
+});
+
+
+noButton.addEventListener('click', function(){
+    console.log("No i dont want ");
+});
+
+// add(2000,3000);
+
+// function add(a,b)
+// {   
+//    alert(a+b);
+// }
